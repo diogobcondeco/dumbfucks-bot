ba1ba33c1d2121d5aa636d6d15ae87ef41d18ab5
Diogo Condêço <hello@diogobcondeco.com>
2022-10-23 23:18:52 +0100

    small refractor and prints added

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv('TOKEN')
 client = discord.Client(intents=discord.Intents.default())
 
 greetings = ['hello', 'hi', 'hey']
-welcome_channel = 963791333707894925
+channel_ids = {'welcome': 963791333707894925}
 
 
 @client.event
@@ -19,11 +19,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print(f'{message.author}, just send the message: {message.content}')
     if message.author == client.user:
         return
 
     if message.author.id == 379048417353269249:
-        await message.channel.send('CA GANDA NOOB')
+        for i in range(10):
+            if i == 1:
+                await message.channel.send('CA GANDA NOOB')
 
     if any(word in message.content for word in greetings):
         await message.channel.send('Hello!')
@@ -31,7 +34,8 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(welcome_channel)
+    print(member.name, 'just joined!')
+    channel = client.get_channel(channel_ids['welcome'])
     role = discord.utils.get(member.server.roles, id='1033854993914150984')
 
     await channel.send(f'Hi {member.name}, welcome to Dumbfucks!')
@@ -39,7 +43,8 @@ async def on_member_join(member):
 
 
 async def on_member_leave(member):
-    channel = client.get_channel(welcome_channel)
+    print(member.name, 'just left!')
+    channel = client.get_channel(channel_ids['welcome'])
     await channel.send(f'Goodbye {member.name}, it was fun while it lasted')
 
 client.run(TOKEN)
