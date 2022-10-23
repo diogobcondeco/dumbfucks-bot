eb84662f683368a4db806a9b09ef75b7b2c58383
Diogo Condêço <hello@diogobcondeco.com>
2022-10-23 22:32:03 +0100

    cleaned some code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 
@@ -11,6 +13,7 @@ greetings = ['hello', 'hi', 'hey']
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
 
 
 @client.event
@@ -18,9 +21,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    msg = message.content
+    if message.author.id == 379048417353269249:
+        await message.channel.send('CA GANDA NOOB')
 
-    if any(word in msg for word in greetings):
+    if any(word in message.content for word in greetings):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(963791333707894925)
+    role = discord.utils.get(member.server.roles, id='1033854993914150984')
+
+    await channel.send(f'Hi {member.name}, welcome to Dumbfucks!')
+    await client.add_roles(member, role)
+
+
+async def on_member_leave(member):
+    channel = client.get_channel(963791333707894925)
+    await channel.send(f'Goodbye {member.name}, it was fun while it lasted')
+
+client.run(TOKEN)
