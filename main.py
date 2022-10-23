a328c06b5081ef1ac7c1a82d4317c20f9afbfeea
Diogo Condêço <hello@diogobcondeco.com>
2022-10-23 21:30:20 +0100

    configured dotenv

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import discord
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
+
+greetings = ['hello', 'hi', 'hey']
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if message.content.startswith('$hello'):
+
+    msg = message.content
+
+    if any(word in msg for word in greetings):
         await message.channel.send('Hello!')
 
 client.run(os.getenv('TOKEN'))
